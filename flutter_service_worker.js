20a3dd57869cad44c412cf261e0dc705d0c8c5de
hamza hossam <hamza.says2017@gmail.com>
2025-11-20 21:13:39 +0200

    web version is ready

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c0893c9bcf34e6511cf630d467a1d4ec
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "5c4f6bc9884a5bfcf815ee49c13b96ef",
+"flutter_bootstrap.js": "b1825b4aa447199e8b59033846e3743b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
